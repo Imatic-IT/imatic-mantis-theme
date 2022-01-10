469c07e3cbad2ff48b5db72a1a48dcaf95072d1b
Jan Pekař <jan.pekar@imatic.cz>
2022-01-10 14:13:22 +0100

    Set smaler H element not only for bugnotes but also for bug-description

--- ImaticTheme.php
+++ ImaticTheme.php
@@ -2,29 +2,29 @@
 
 class ImaticThemePlugin extends MantisPlugin
 {
-	public function register(): void
-	{
-		$this->name = 'Imatic theme';
-		$this->description = 'Theme';
-		$this->version = '0.0.1';
-		$this->requires = [
-			'MantisCore' => '2.0.0',
-		];
+  public function register(): void
+  {
+    $this->name = 'Imatic theme';
+    $this->description = 'Theme';
+    $this->version = '0.0.2';
+    $this->requires = [
+      'MantisCore' => '2.0.0',
+    ];
 
-		$this->author = 'Imatic Software s.r.o.';
-		$this->contact = 'info@imatic.cz';
-		$this->url = 'https://www.imatic.cz/';
-	}
+    $this->author = 'Imatic Software s.r.o.';
+    $this->contact = 'info@imatic.cz';
+    $this->url = 'https://www.imatic.cz/';
+  }
 
-	public function hooks(): array
-	{
-		return [
-			'EVENT_LAYOUT_RESOURCES' => 'layout_resources_hook',
-		];
-	}
+  public function hooks(): array
+  {
+    return [
+      'EVENT_LAYOUT_RESOURCES' => 'layout_resources_hook',
+    ];
+  }
 
-	public function layout_resources_hook()
-	{
-		return '<link rel="stylesheet" type="text/css" href="' . plugin_file('styles.css') . '&v=' . $this->version . '" />';
-	}
+  public function layout_resources_hook()
+  {
+    return '<link rel="stylesheet" type="text/css" href="' . plugin_file('styles.css') . '&v=' . $this->version . '" />';
+  }
 }
