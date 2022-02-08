0700940e62e685223aec57e8012bf79bbd4eba27
imatic-preclik <90459913+imatic-preclik@users.noreply.github.com>
2022-02-08 05:29:54 +0100

    Update navbar.js

--- files/navbar.js
+++ files/navbar.js
@@ -1,30 +1,17 @@
 // aby existovaly prazdne promenne
-var modtitle="";
-var bug2clip="";
+var modtitle="nothing";
+var bug2clip="nothing";
 
-// az se nahraji vsechny class navbar-brand pridej id 'mantis-title' k prvni z nich
-document.getElementsByClassName('navbar-brand').onload = function(){
+document.body.onload = newtitle();
+
+function newtitle(){
+  // az se nahraje body pridej id 'mantis-title' k prvni z classes navbar-brand, 'bug-index' k druhe z classes bug-id
   document.getElementsByClassName('navbar-brand')[0].id='mantis-title';
   var modtitle=document.getElementById('mantis-title');
-  // a zkus zmenu nadpisu
-  newtitle();
-}
-// az se nahraji vsechny class bug-id pridej id 'bug-index' k druhe z nich
-document.getElementsByClassName('bug-id').onload=function(){
-  document.getElementsByClassName('bug-id')[0].id='bug-index';
+  document.getElementsByClassName('bug-id')[1].id='bug-index';
   var bug2clip=document.getElementById('bug-index');
-  //a zkus zmenu nadpisu
-  newtitle();
-}
-// pokud uz ID prvku mantis-title a zaroven bug-id ma hodnotu...
-function newtitle(){
-  // ...vyrob nove html do nadpisu i s javascriptem pro clipboard
-  if (modtitle.length > 0 && bug2clip.length > 0) {
-    modtitle.outerHTML=modtitle.outerHTML + '<span id="copy2clip" class="navbar-brand">['+bug2clip.innerText+']</span>';
-    copy2clip.onclick=function() {navigator.clipboard.writeText(bug2clip.innerText);}
-  }
-  // debug
-  else {
-    document.getElementsByClassName('navbar-brand')[0].innerText="nothing there";
-  }
+  //...vyrob nove html do nadpisu 
+  modtitle.outerHTML=modtitle.outerHTML + '<span id="copy2clip" class="navbar-brand">['+bug2clip.innerText+']</span>';
+  // klik na nadpis = javascript copy bug-index text to clipboard
+  copy2clip.onclick=function() {navigator.clipboard.writeText(bug2clip.innerText);}
 }
