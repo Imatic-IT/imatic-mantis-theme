a2a00e0e0c520931af748b0f75f969e62c176b06
imatic-preclik <90459913+imatic-preclik@users.noreply.github.com>
2022-02-08 09:30:42 +0100

    pridano opakovani 5x za sec dokud se nepovede

--- files/navbar.js
+++ files/navbar.js
@@ -2,16 +2,19 @@
 var modtitle="nothing";
 var bug2clip="nothing";
 
-document.body.onload = newtitle();
+document.onload=newtitle();
 
 function newtitle(){
   // az se nahraje body pridej id 'mantis-title' k prvni z classes navbar-brand, 'bug-index' k druhe z classes bug-id
-  document.getElementsByClassName('navbar-brand')[0].id='mantis-title';
-  var modtitle=document.getElementById('mantis-title');
-  document.getElementsByClassName('bug-id')[1].id='bug-index';
-  var bug2clip=document.getElementById('bug-index');
+  try {
+    document.getElementsByClassName('navbar-brand')[0].id='mantis-title';
+    var modtitle=document.getElementById('mantis-title');
+    document.getElementsByClassName('bug-id')[1].id='bug-index';
+    var bug2clip=document.getElementById('bug-index');
   //...vyrob nove html do nadpisu 
-  modtitle.outerHTML+='<span id="copy2clip" class="navbar-brand">['+bug2clip.innerText+']</span>';
+    modtitle.outerHTML+='<span id="copy2clip" class="navbar-brand">['+bug2clip.innerText+']</span>';
   // klik na nadpis = javascript copy bug-index text to clipboard
-  copy2clip.onclick=function() {navigator.clipboard.writeText(bug2clip.innerText);}
-}
+    copy2clip.onclick=function() {navigator.clipboard.writeText(bug2clip.innerText);}
+  }
+  catch {setTimeout(newtitle(), 200)}
+} 
