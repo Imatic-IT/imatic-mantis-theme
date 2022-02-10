6c41e039d1f804dcddb9a4f09177dc7bd0d9678f
imatic-preclik <90459913+imatic-preclik@users.noreply.github.com>
2022-02-10 15:09:39 +0100

    verze otestovana v tampermonkey

--- files/navbar.js
+++ files/navbar.js
@@ -2,19 +2,15 @@
 var modtitle="nothing";
 var bug2clip="nothing";
 
-document.onload=newtitle();
-
-function newtitle(){
-  // az se nahraje body pridej id 'mantis-title' k prvni z classes navbar-brand, 'bug-index' k druhe z classes bug-id
-  try {
-    document.getElementsByClassName('navbar-brand')[0].id='mantis-title';
-    var modtitle=document.getElementById('mantis-title');
-    document.getElementsByClassName('bug-id')[1].id='bug-index';
-    var bug2clip=document.getElementById('bug-index');
-  //...vyrob nove html do nadpisu 
-    modtitle.outerHTML+='<span id="copy2clip" class="navbar-brand">['+bug2clip.innerText+']</span>';
-  // klik na nadpis = javascript copy bug-index text to clipboard
-    copy2clip.onclick=function() {navigator.clipboard.writeText(bug2clip.innerText);}
-  }
-  catch {setTimeout(newtitle(), 200)}
-} 
+//pridej id 'mantis-title' k prvni z classes navbar-brand
+var navbarid=document.getElementsByClassName('navbar-brand')[0];
+navbarid.id="mantis-title";
+var modtitle=document.getElementById('mantis-title');
+// a 'bug-index' k druhe z classes bug-id
+var bugid=document.getElementsByClassName('bug-id')[1];
+bugid.id='bug-index';
+var bug2clip=document.getElementById('bug-index');
+//...vyrob nove html do nadpisu
+modtitle.outerHTML+='<span id="copy2clip" class="navbar-brand">['+bug2clip.innerText+']</span>';
+// klik na nadpis = javascript copy bug-index text to clipboard
+copy2clip.onclick=function() {navigator.clipboard.writeText(bug2clip.innerText);}
