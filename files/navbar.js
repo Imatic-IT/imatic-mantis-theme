c94d698bd1b064178e949d04ab64b5a0cfc1764f
imatic-preclik <90459913+imatic-preclik@users.noreply.github.com>
2022-02-17 09:52:43 +0100

    Update navbar.js

--- files/navbar.js
+++ files/navbar.js
@@ -1,16 +1,22 @@
-// aby existovaly prazdne promenne
-var modtitle="nothing";
-var bug2clip="nothing";
-
 //pridej id 'mantis-title' k prvni z classes navbar-brand
-var navbarid=document.getElementsByClassName('navbar-brand')[0];
-navbarid.id="mantis-title";
-var modtitle=document.getElementById('mantis-title');
-// a 'bug-index' k druhe z classes bug-id
-var bugid=document.getElementsByClassName('bug-id')[1];
-bugid.id='bug-index';
-var bug2clip=document.getElementById('bug-index');
-//...vyrob nove html do nadpisu
-modtitle.outerHTML+='<span id="copy2clip" class="navbar-brand">['+bug2clip.innerText+']</span>';
-// klik na nadpis = javascript copy bug-index text to clipboard
-copy2clip.onclick=function() {navigator.clipboard.writeText(bug2clip.innerText);}
+    var navbarid=document.getElementsByClassName('navbar-brand')[0];
+    navbarid.id="mantis-title";
+    var modtitle=document.getElementById('mantis-title');
+    // a 'bug-index' k druhe z classes bug-id
+    var bugid=document.getElementsByClassName('bug-id')[1];
+    bugid.id='bug-index';
+    var bug2clip=document.getElementById('bug-index');
+    // dalsi podle classname
+    var recents=document.getElementsByClassName('nav-recent hidden-xs')[0];
+    recents.firstChild.remove();
+    //tydle maji ID takze podstatne jednodussi, rovnou je priradim.
+    var searchbox=document.getElementById('nav-search');
+    var breadcrumbs=document.getElementById('breadcrumbs');
+    //...vyrob nove html do nadpisu
+    modtitle.outerHTML+='<div class="navbar-header navbar-brand"><span id="copy2clip" class="smaller-75">['+bug2clip.innerText+']</span></div>'+
+        '<div class="navbar-brand" style="width: 160px; padding-top: 0px; padding-bottom: 0px; line-height: 7px;"><span style="font-size: 12px;">'+recents.innerHTML+'</span></div>'+
+        '<div class="navbar-header navbar-brand" style="width: 80px;"><span class="smaller-75">'+searchbox.innerHTML+'</span></div>';
+    //znicit element breadcrumbs z puvodni pozice
+    document.getElementById('breadcrumbs').remove();
+    //registrovat udalost klik na nadpis a vyrobit funkci = javascript copy bug-index text to clipboard
+    copy2clip.onclick=function() {navigator.clipboard.writeText(bug2clip.innerText);}
