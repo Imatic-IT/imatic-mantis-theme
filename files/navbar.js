05db67141d47148677c3a353606566b3555314a7
imatic-preclik <90459913+imatic-preclik@users.noreply.github.com>
2022-02-18 11:52:26 +0100

    verze cekajici na DOM

--- files/navbar.js
+++ files/navbar.js
@@ -1,17 +1,19 @@
-//pridej id 'mantis-title' k prvni z classes navbar-brand
-    var navbarid=document.getElementsByClassName('navbar-brand')[0];
+  document.addEventListener("DOMContentLoaded", newnavbar());
+  function newnavbar(){
+    //pridej id 'mantis-title' k prvni z classes navbar-brand at se s nim da neco delat.
+    let navbarid=document.getElementsByClassName('navbar-brand')[0];
     navbarid.id="mantis-title";
-    var modtitle=document.getElementById('mantis-title');
+    let modtitle=document.getElementById('mantis-title');
     // a 'bug-index' k druhe z classes bug-id
-    var bugid=document.getElementsByClassName('bug-id')[1];
+    let bugid=document.getElementsByClassName('bug-id')[1];
     bugid.id='bug-index';
-    var bug2clip=document.getElementById('bug-index');
+    let bug2clip=document.getElementById('bug-index');
     // dalsi podle classname
-    var recents=document.getElementsByClassName('nav-recent hidden-xs')[0];
+    let recents=document.getElementsByClassName('nav-recent hidden-xs')[0];
     recents.firstChild.remove();
     //tydle maji ID takze podstatne jednodussi, rovnou je priradim.
-    var searchbox=document.getElementById('nav-search');
-    var breadcrumbs=document.getElementById('breadcrumbs');
+    let searchbox=document.getElementById('nav-search');
+    let breadcrumbs=document.getElementById('breadcrumbs');
     //...vyrob nove html do nadpisu
     modtitle.outerHTML+='<div class="navbar-header navbar-brand"><span id="copy2clip" class="smaller-75">['+bug2clip.innerText+']</span></div>'+
         '<div class="navbar-brand" style="width: 160px; padding-top: 0px; padding-bottom: 0px; line-height: 7px;"><span style="font-size: 12px;">'+recents.innerHTML+'</span></div>'+
@@ -20,3 +22,4 @@
     document.getElementById('breadcrumbs').remove();
     //registrovat udalost klik na nadpis a vyrobit funkci = javascript copy bug-index text to clipboard
     copy2clip.onclick=function() {navigator.clipboard.writeText(bug2clip.innerText);}
+  }
