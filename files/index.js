ce9690dec9f3aab46eec937909131db76915d051
Matej Brodziansky <matejbrodziansky@gmail.com>
2026-08-20 15:11:44 +0200

    Revert "fix: also move slack reminder button into widget-toolbox row"
    This reverts commit 9c01265394905a687dc34370dba0325e84001654.

--- files/index.js
+++ files/index.js
@@ -1,21 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const newButton = document.querySelector("#imaticEditBugButton");
     const widgetToolbox = document.querySelector(".widget-toolbox");
-    if (!widgetToolbox) {
-        return;
-    }
 
-    const editButton = document.querySelector("#imaticEditBugButton");
-    if (editButton) {
-        widgetToolbox.insertBefore(editButton, widgetToolbox.firstChild);
-    }
-
-    const slackButton = document.querySelector("#notifyToSlack");
-    if (slackButton) {
-        const pullRight = widgetToolbox.querySelector(".pull-right");
-        if (pullRight) {
-            widgetToolbox.insertBefore(slackButton, pullRight);
-        } else {
-            widgetToolbox.appendChild(slackButton);
-        }
+    if (newButton && widgetToolbox) {
+        widgetToolbox.insertBefore(newButton, widgetToolbox.firstChild);
     }
 });
